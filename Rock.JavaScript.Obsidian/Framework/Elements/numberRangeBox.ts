149cf524b9ae920da636edf5e265ef09984a9700
Joe <joe@triumph.tech>
2022-03-23 10:34:00 -0500

    - Remove computed value from being a normal data property.

--- Rock.JavaScript.Obsidian/Framework/Elements/numberRangeBox.ts
+++ Rock.JavaScript.Obsidian/Framework/Elements/numberRangeBox.ts
@@ -66,8 +66,7 @@ export default defineComponent({
             internalValue: {
                 lower: "",
                 upper: ""
-            },
-            validationValue: ""
+            }
         };
     },
 
